708925b71b2d98e41a4d1bdc3ca157c0500fa436
Brett Reinhard <brett@reinhards.us>
2017-11-10 20:28:38 +0000

    rebuild pages at 8256fc5

--- constants.js
+++ constants.js
@@ -4,8 +4,8 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 // Hostname to be deployed to
-var hostname = 'https://api-math.herokuapp.com:80';
-// const hostname = 'http://localhost:8080';
+// const hostname = 'https://api-math.herokuapp.com:';
+var hostname = 'http://localhost:8080';
 var links = {
     root: '' + hostname
 };
